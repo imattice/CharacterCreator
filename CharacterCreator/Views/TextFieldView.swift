91ca427ef576180c0e88ec4ffef4f71953e62951
Ike Mattice <ikemattice@icloud.com>
2018-11-28 21:48:56 -0800

    add image selection view

--- CharacterCreator/Views/TextFieldView.swift
+++ CharacterCreator/Views/TextFieldView.swift
@@ -18,16 +18,16 @@ class TextFieldView: UIView {
 	private let underline 	= UIView()
 	private let defaultFrame = CGRect(x: 0, y: 0, width: 150, height: 30)
 
-	init(placeholder: String) {
-		self.placeholder = placeholder
-		super.init(frame: defaultFrame)
-
-		config()  //this might crash, due to the object not created at time setting delegate in config
-	}
-
-	convenience init() {
-		self.init(placeholder: "")
-	}
+//	init(placeholder: String) {
+//		self.placeholder = placeholder
+//		super.init(frame: defaultFrame)
+//
+//		config()  //this might crash, due to the object not created at time setting delegate in config
+//	}
+//
+//	convenience init() {
+//		self.init(placeholder: "")
+//	}
 
 	required init?(coder aDecoder: NSCoder) {
 		super.init(coder: aDecoder)
@@ -39,10 +39,8 @@ class TextFieldView: UIView {
 
 		textField.placeholder = placeholder
 		textField.delegate = self
-		textField.translatesAutoresizingMaskIntoConstraints = false
 
 		underline.backgroundColor = Character.default.class.color().darkColor()
-		underline.translatesAutoresizingMaskIntoConstraints	= false
 
 		addSubview(underline)
 		addSubview(textField)
@@ -54,15 +52,16 @@ class TextFieldView: UIView {
 		let viewDict = ["underline": 	underline,
 						"textField": 	textField,
 						"self":			self ]
+
+		underline.translatesAutoresizingMaskIntoConstraints	= false
 		let H_underline = NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[underline]-10-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: viewDict)
 		let V_underline = NSLayoutConstraint.constraints(withVisualFormat: "V:[textField][underline(3)]-4-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: viewDict)
 
+		textField.translatesAutoresizingMaskIntoConstraints = false
 		let H_textField = NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[textField]-10-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: viewDict)
 		let V_textField = NSLayoutConstraint.constraints(withVisualFormat: "V:[textField(>=5)]", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: viewDict)
 
-		let H_self = NSLayoutConstraint.constraints(withVisualFormat: "V:[self(50)]", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: viewDict)
-
-		let constraints = H_underline + V_underline + H_textField + V_textField + H_self
+		let constraints = H_underline + V_underline + H_textField + V_textField
 
 		self.addConstraints(constraints)
 	}
@@ -73,13 +72,34 @@ extension TextFieldView: UITextFieldDelegate {
 }
 
 class ImageSelectionView: UIView {
-	@IBInspectable let imageView = UIImageView()
+	private let imageView = UIImageView()
+
+	@IBInspectable var image: UIImage? {
+		didSet {
+			imageView.image = image		}}
+
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+
+		config()
+	}
+
+	private func config() {
+		backgroundColor = .brown
+
+		addSubview(imageView)
+
+		layoutViews()
+	}
 
 	private func layoutViews() {
-		let viewDict = ["imageView": imageView]
 
-		let H_imageView = NSLayoutConstraint.constraints(withVisualFormat: "", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: viewDict)
-		let V_imageView = NSLayoutConstraint.constraints(withVisualFormat: "", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: viewDict)
+		let viewDict = ["imageView"		: imageView,
+						"self"			: self ]
+
+		imageView.translatesAutoresizingMaskIntoConstraints = false
+		let H_imageView = NSLayoutConstraint.constraints(withVisualFormat: "[imageView(<=75)]", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: viewDict)
+		let V_imageView = NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[imageView(<=75)]-0-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: viewDict)
 
 		let constraints = H_imageView + V_imageView
 
