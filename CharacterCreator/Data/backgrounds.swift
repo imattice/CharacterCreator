7fae74818388c8e0f65d37d3d99fb54303356449
Ike Mattice <ikemattice@icloud.com>
2019-04-04 22:03:11 -0700

    add data for bonds, ideals and flaws to backgrounds

--- CharacterCreator/Data/backgrounds.swift
+++ CharacterCreator/Data/backgrounds.swift
@@ -13,6 +13,30 @@ let backgroundData: [String : Any] = [
 		"equipment": ["A holy symbol", "a prayer book", "5 sticks of incense", "vestments", "a set of common clothes", "coin pouch (15gp)"],
 		"reputation": [
 			"Shelter of the Failthful": "Living a life devoted to the temple of your diety, you can expect to be welcomed and granted shelter within any community that shares your beliefs.  The servants there will gladly heal your wounds and support you where they can." ],
+		"ideals": [
+			"Tradition: The ancient traditions of worship and sacrifice must be preserved and upheld.",
+			"Charity: I always try to help those in need, no matter what the personal cost.",
+			"Change: We must help bring about the changes the gods are constantly working in the world.",
+			"Power: I hope to one day rise to the top of my faith's religious hierarchy.",
+			"Faith: I trust that my deity will guide my actions, I have faith that if I work hard, things will go well.",
+			"Aspiration: I seek to prove myself worthy of my god's favor by matching my actions against their teachings.",
+		],
+		"bonds": [
+			"I would die to recover an ancient relic of my faith that was lost long ago.",
+			"I will someday get revenge on the corrupt temple hierarchy who branded me a heretic.",
+			"I owe my life to the priest who took me in when my parents died.",
+			"Everything I do is for the common people.",
+			"I will do anything to protect the temple where I served.",
+			"I seek to preserve a sacred text that my enemies consider heretical and seek to destroy.",
+		],
+		"flaws": [
+			"I judge others harshly, and myself even more severely.",
+			"I put too much trust in those who wield power within my temple's hierarchy.",
+			"My piety sometimes leads me to blindly trust those that profess faith in my god.",
+			"I am inflexible in my thinking.",
+			"I am suspicious of strangers and expect the worst of them.",
+			"Once I pick a goal, I become obsessed with it to the detriment of everything else in my life.",
+		],
 		"description" : "" ],
 	"criminal": [
 		"skills": ["deception", "stealth"],
@@ -23,6 +47,31 @@ let backgroundData: [String : Any] = [
 		"specialty": ["Blackmailer", "Burgler", "Enforcer", "Fence", "Highway Robber", "Hired Killer", "Pickpocket", "Smuggler"],
 		"reputation": [
 			"Criminal Contact": "Your primary contact can serve as a liason to other criminals and organizations that may find your skills... useful.  Using this network of messagers, you can quickly get your words across great distances. " ],
+		"ideals": [
+			"Honor: I don't steal from others in the trade.",
+			"Freedom: Chains are meant to be broken, as are those who would forge them.",
+			"Charity: I steal from the wealthy so that I can help people in need.",
+			"Greed: I will do whatever it takes to become wealthy.",
+			"People: I'm loyal to my friends, not to any ideals, and everyone else can take a trip down the Styx for all I care.",
+			"Redemption: There's a spark of good in everyone.",
+		],
+		"bonds": [
+			"I'm trying to pay off an old debt I owe to a generous benefactor.",
+			"My ill-gotten gains go to support my family.",
+			"Something important was taken from me, and I aim to steal it back.",
+			"I will become the greatest thief that ever lived.",
+			"I'm guilty of a terrible crime. I hope I can redeem myself for it.",
+			"Someone I loved died because of a mistake I made. That will never happen again.",
+		],
+		"flaws": [
+			"When I see something valuable, I can't think about anything but how to steal it.",
+			"When faced with a choice between money and my friends, I usually choose the money.",
+			"If there's a plan, I'll forget it. If I don't forget it, I'll ignore it.",
+			"I have a \"tell\" that reveals when I'm lying.",
+			"I turn tail and run when things look bad.",
+			"An innocent person is in prison for a crime that I committed. I'm okay with that.",
+
+		],
 		"description": "" ],
 	"folk hero": [
 		"skills": ["animal handling", "survival"],
@@ -32,6 +81,30 @@ let backgroundData: [String : Any] = [
 		"equipment": ["artisan's tools", "a shovel", "an iron pot", "a set of common clothes", "coin pouch (10gp)"],
 		"reputation": [
 			"Rustic Hospitality": "The peasants are your people and you are known to offer what help you can.  In return, common folk are willing to reciprocate that aid, granting you food, water, shelter - even a place to hide, if needed." ],
+		"ideals": [
+			"Respect: People deserve to be treated with dignity and respect.",
+			"Fairness: No one should get preferential treatment before the law, and no one is above the law.",
+			"Freedom: Tyrants must not be allowed to oppress the people.",
+			"Might: If I become strong, I can take what I want– what I deserve.",
+			"Sincerity: There's no good in pretending to be something I'm not. (Neutral)",
+			"Destiny: Nothing and no one can steer me away from my higher calling. (Any)",
+		],
+		"bonds": [
+			"I have a family, but I have no idea where they are. One day, I hope to see them again.",
+			"I worked the land, I love the land, and I will protect the land.",
+			"A proud noble once gave me a horrible beating, and I will take my revenge on any bully I encounter.",
+			"My tools are symbols of my past life, and I carry them so that I will never forget my roots.",
+			"I protect those who cannot protect themselves.",
+			"I wish my childhood sweetheart had come with me to pursue my destiny.",
+		],
+		"flaws": [
+			"The tyrant who rules my land will stop at nothing to see me killed.",
+			"I'm convinced of the significance of my destiny, and blind to my shortcomings and the risk of failure.",
+			"The people who knew me when I was young know my shameful secret, so I can never go home again.",
+			"I have a weakness for the vices of the city, especially hard drink.",
+			"Secretly, I believe that things would be better if I were a tyrant lording over the land.",
+			"I have trouble trusting in my allies.",
+		],
 		"description" : "" ],
 	"noble": [
 		"skills": ["history", "persuasion"],
@@ -42,6 +115,30 @@ let backgroundData: [String : Any] = [
 		"equipment": ["a signet ring", "a scroll of pedigree" ],
 		"reputation": [
 			"Position of Privilege": "You carry yourself with honor and wealth - people know you you are wherever you go.  In an effort to stay in your good graces, people will bend to your will easily and assume that you are in the right place.  Using your status, you can even request to meet with the important people of the locale." ],
+		"ideals": [
+			"Respect: Respect is due to me because of my position, but all people regardless of station deserve to be treated with dignity.",
+			"Responsibility: It is my duty to respect the authority of those above me, just as those below me must respect mine,",
+			"Independence: I must prove that I can handle myself without the coddling of my family.",
+			"Power: If I can attain more power, no one will tell me what to do.",
+			"Family: Blood runs thicker than water.",
+			"Noble Obligation: It is my duty to protect and care for the people beneath me. (Good)",
+		],
+		"bonds": [
+			"I will face any challenge to win the approval of my family.",
+			"My house's alliance with another noble family must be sustained at all costs.",
+			"Nothing is more important than the other members of my family.",
+			"I am in love with the heir of a family that my family despises.",
+			"My loyalty to my sovereign is unwavering.",
+			"The common folk must see me as a hero of the people.",
+		],
+		"flaws": [
+			"I secretly believe that everyone is beneath me.",
+			"I hide a truly scandalous secret that could ruin my family forever.",
+			"I too often hear veiled insults and threats in every word addressed to me, and I'm quick to anger.",
+			"I have an insatiable desire for carnal pleasures.",
+			"In fact, the world does revolve around me.",
+			"By my words and actions, I often bring shame to my family.",
+		],
 		"description" : "" ],
 	"sage": [
 		"skills": ["arcana", "history"],
@@ -52,6 +149,30 @@ let backgroundData: [String : Any] = [
 		"specialty": ["Alchemist", "Astronomer", "Discredited Academic", "Librarian", "Professor", "Researcher", "Wizard's Apprentice", "Scribe"],
 		"reputation": [
 			"Researcher": "Your familiarity with finding knowledge gives you an edge when looking for a specific piece of information.  You know how to request access to repositories of knowledge, such as libraries, universities, or other well-read individuals." ],
+		"ideals": [
+			"Knowledge: The path to power and self-improvement is through knowledge.",
+			"Beauty: What is beautiful points us beyond itself toward what is true.",
+			"Logic: Emotions must not cloud our logical thinking.",
+			"No Limits: Nothing should fetter the infinite possibility inherent in all existence.",
+			"Power: Knowledge is the path to power and domination.",
+			"Self-Improvement: The goal of a life of study is the betterment of oneself.",
+		],
+		"bonds": [
+			"It is my duty to protect my students.",
+			"I have an ancient text that holds terrible secrets that must not fall into the wrong hands.",
+			"I work to preserve a library, university, scriptorium, or monastery.",
+			"My life's work is a series of tomes related to a specific field of lore.",
+			"I've been searching my whole life for the answer to a certain question.",
+			"I sold my soul for knowledge. I hope to do great deeds and win it back.",
+		],
+		"flaws": [
+			"I am easily distracted by the promise of information.",
+			"Most people scream and run when they see a demon. I stop and take notes on its anatomy.",
+			"Unlocking an ancient mystery is worth the price of a civilization.",
+			"I overlook obvious solutions in favor of complicated ones.",
+			"I speak without really thinking through my words, invariably insulting others.",
+			"I can't keep a secret to save my life, or anyone else's.",
+		],
 		"description" : "" ],
 	"soldier": [
 		"skills": ["athletics", "intimidatation"],
@@ -62,6 +183,30 @@ let backgroundData: [String : Any] = [
 		"specialty": ["Officer", "Scout", "Infantry", "Cavalry", "Healer", "Quatermaster", "Standard Bearer", "Support Staff"],
 		"reputation": [
 			"Researcher": "You come from a world of order and dicipline.  Other militant leaders recognize your status and accomplishments, and you can impose your influance on any other soldier of a lower rank.  You can usually find respite at a friendly military barracks or encampment." ],
+		"ideals": [
+			"Greater Good: Our lot is to lay down our lives in defense of others.",
+			"Responsibility: I do what I must and obey just authority.",
+			"Independence: When people follow orders blindly, they embrace a kind of tyranny.",
+			"Might: In life as in war, the stronger force wins.",
+			"Live and Let Live: Ideals aren't worth killing over or going to war for.",
+			"Nation: My city, nation, or people are all that matter."
+		],
+		"bonds": [
+			"I would still lay down my life for the people I served with.",
+			"Someone saved my life on the battlefield. To this day, I will never leave a friend behind.",
+			"My honor is my life.",
+			"I'll never forget the crushing defeat my company suffered or the enemies who dealt it.",
+			"Those who fight beside me are those worth dying for.",
+			"I fight for those who cannot fight for themselves.",
+		],
+		"flaws": [
+			"The monstrous enemy we faced in battle still leaves me quivering with fear.",
+			"I have little respect for anyone who is not a proven warrior.",
+			"I made a terrible mistake in battle that cost many lives-- and I would do anything to keep that mistake secret.",
+			"My hatred of my enemies is blind and unreasoning.",
+			"I obey the law, even if the law causes misery.",
+			"I'd rather eat my armor than admit when I'm wrong.",
+		],
 		"description" : "" ],
 ]
 
