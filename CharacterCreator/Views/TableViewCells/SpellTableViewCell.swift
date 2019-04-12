199bd69d5d349a039b7f7c545fd64e112092cb8f
Ike Mattice <ikemattice@icloud.com>
2019-04-12 12:48:23 -0700

    add expanded value to the SpellTableViewData to trigger the cell expansion state when the cell is tapped

--- CharacterCreator/Views/TableViewCells/SpellTableViewCell.swift
+++ CharacterCreator/Views/TableViewCells/SpellTableViewCell.swift
@@ -12,11 +12,49 @@ class SpellTableViewCell: UITableViewCell {
 	@IBOutlet weak var titleLabel: UILabel!
 	@IBOutlet weak var iconView: UIImageView!
 	@IBOutlet weak var descriptionLabel: UILabel!
+	@IBOutlet weak var componentLabel: UILabel!
 
+	var spell: Spell?
+//	var expanded: Bool = false
 
-	func configure(for spell: Spell) {
+//	func configure(for spell: Spell) {
+//		titleLabel.text 			= spell.name
+//		descriptionLabel.text		= spell.description()
+//		iconView.image				= UIImage(named: spell.school)
+//
+//		componentLabel.text			= expanded ? spell.components			: ""
+//
+//		self.spell = spell
+//	}
+
+	func configure(for spellData: (expanded: Bool, spell: Spell)) {
+		let spell 		= spellData.spell
+		let expanded 	= spellData.expanded
+
 		titleLabel.text 			= spell.name
 		descriptionLabel.text		= spell.description()
 		iconView.image				= UIImage(named: spell.school)
+
+		componentLabel.text			= expanded ? spell.components			: ""
+
+		self.spell = spell
 	}
+//	func open() {
+//		print("will open: \(spell?.components)")
+//		guard let spell = spell else { return }
+//		componentLabel.text		= spell.components
+//
+//		print("did open")
+//	}
+//
+//	func close() {
+//		componentLabel.text		= ""
+//	}
+
+//	func toggle() {
+//		if isSelected {
+//			close()		}
+//		else {
+//			open()		}
+//	}
 }
