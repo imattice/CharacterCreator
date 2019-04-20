b0b270098a4e018d2a308dd14df7ca34a1e080f4
Ike Mattice <ikemattice@icloud.com>
2019-04-19 21:31:41 -0700

    add delegate functionality to the relationshipsTextAreaView and add some reseting functionality to the cancel button for the alignment view

--- CharacterCreator/Views/DetailViews/SocialDetailView.swift
+++ CharacterCreator/Views/DetailViews/SocialDetailView.swift
@@ -20,8 +20,10 @@ class SocialDetailView: XibView {
 	override func config() {
 		pickerView.delegate 	= self
 		pickerView.dataSource 	= self
+		relationshipsTextAreaView.textView.delegate	= self
 
 		alignmentTextFieldView.textField.inputView = pickerView
+		alignmentTextFieldView.tintColor			= .clear
 
 		alignmentTextFieldView.textField.addToolbar(self, onOk: .okSelected, onCancel: .cancelSelected)
 		relationshipsTextAreaView.textView.addToolbar(self, onOk: .okSelected, onCancel: .cancelSelected)
@@ -34,14 +36,73 @@ class SocialDetailView: XibView {
 	}
 
 	@objc func cancelSelected() {
-		UIResponder.current?.resignFirstResponder()
+		guard let responder = UIResponder.current else { return }
+
+		switch responder {
+		case alignmentTextFieldView.textField:
+			pickerView.selectRow(0, inComponent: 0, animated: true)
+			alignmentTextFieldView.textField.text = alignmentTextFieldView.placeholder
+			alignmentTextFieldView.textField.textColor = UIColor.lightGray
+
+		default:
+			break
+		}
+
+		responder.resignFirstResponder()
 	}
 
 	func setCharacterDetail() {
-		
+		guard let responder = UIResponder.current else { return }
+
+		switch responder {
+		case alignmentTextFieldView.textField as UITextField:
+			guard let alignment = alignmentTextFieldView.textField.text else { return }
+			Character.current.flavorText.alignment = alignment
+
+		case relationshipsTextAreaView.textView as UITextView:
+			guard let relationships = relationshipsTextAreaView.textView.text else { return }
+			Character.current.flavorText.relationships	= relationships
+		default:
+			break
+		}
 	}
 
 	@IBAction func addLaguage(_ sender: UIButton) {
+	}
+}
+
+extension SocialDetailView: UITextViewDelegate {
+	func textViewDidBeginEditing(_ textView: UITextView) {
+	}
+	func textViewDidChangeSelection(_ textView: UITextView) {
+		if textView.textColor == UIColor.lightGray {
+			textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+		}
+	}
+
+	func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+		let currentText:String = textView.text
+		let updatedText = (currentText as NSString).replacingCharacters(in: range, with: text)
+
+		//if updated text view will be empty, add the placeholder
+		if updatedText.isEmpty {
+			relationshipsTextAreaView.setPlaceholder()
+		}
+
+			//remove the placeholder if the user is about to add text
+		else if textView.textColor == UIColor.lightGray && !text.isEmpty {
+			textView.textColor = UIColor.black
+			textView.text = text
+		}
+			// For every other case, the text should change with the usual behavior...
+		else {
+			return true		}
+
+		// ...otherwise return false since the updates have already been made
+		return false
+	}
+	func textViewDidEndEditing(_ textView: UITextView) {
+		setCharacterDetail()
 	}
 }
 
@@ -71,6 +132,8 @@ extension SocialDetailView: UIPickerViewDelegate, UIPickerViewDataSource {
 	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
 		return alignments.count	}
 }
+
+
 
 fileprivate extension Selector {
 	static let okSelected = #selector(SocialDetailView.okSelected)
