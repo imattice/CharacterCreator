7832caa85f0d44130c1cf25819cb34742f74a9c5
Ike Mattice <ikemattice@icloud.com>
2021-04-07 20:25:05 -0700

    remove deprecated item files

--- CharacterCreator/Models/Item.swift
+++ CharacterCreator/Models/Item.swift
@@ -6,6 +6,11 @@
 //  Copyright © 2018 Ike Mattice. All rights reserved.
 //
 
+class Item {
+    let name: String
+    var count: Int
+}
+
 ///defines an object that can be collected, disposed and used by a character
 class ItemRecord: Record {
     ///An identifier for the item
@@ -119,38 +124,4 @@ class ToolRecord: Item, Record {
     enum Category {
         case artisan, musical, gaming
     }
-}
-
-extension Item: Equatable {
-	static func == (lhs: Item, rhs: Item) -> Bool {
-		return lhs.name == rhs.name
-	}
-}
-
-extension Item {
-//	static let ComponentPouch 	= ItemRecord.record(for: "component pouch")!.item()
-//	static let ArcaneFocus		= ItemRecord.record(for: "arcane focus")!.item()
-//	static let Spellbook		= ItemRecord.record(for: "spellbook")!.item()
-//	static let HolySymbol		= ItemRecord.record(for: "holy symbol")!.item()
-}
-
-
-
-@objcMembers
-class ItemRecord: Object {
-	dynamic var id: String			= UUID().uuidString
-	dynamic var name: String		= ""
-	dynamic var detail: String		= ""
-
-	static func allRecords(in realm: Realm = RealmProvider.itemRecords.realm) -> Results<ItemRecord> {
-		return realm.objects(ItemRecord.self)//.sorted(byKeyPath: "name")
-	}
-
-	static func record(for name: String, in realm: Realm = RealmProvider.itemRecords.realm) -> ItemRecord? {
-		return allRecords().filter({ $0.name == name }).first
-	}
-
-	func item() -> Item {
-		return Item(name, type: .other, detail: detail)
-	}
 }
