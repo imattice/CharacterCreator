981bee8ab01f74aed916b6f91be6c828abe95230
Ike Mattice <ikemattice@icloud.com>
2021-04-09 09:04:08 -0700

    add spellRecord; add spell json data;

--- CharacterCreator/Models/Spell.swift
+++ CharacterCreator/Models/Spell.swift
@@ -6,128 +6,55 @@
 //  Copyright © 2018 Ike Mattice. All rights reserved.
 //
 
-
-//could use additional damage rolls, such as acid arrow, which does 4d4, and then 2d4 next turn
-//targets might be nice, like aid targets 3 specific characters, or acid splash targets 2 creatures within 5 feet of each other
-//add an effect, such as a damage roll, an ac bonus or stat alteration, heal
 import Foundation
-import RealmSwift
+
+///An object holding data for spells
+class SpellRecord: Record {
+    ///An id for the spell
+    let id: String = UUID().uuidString
+    ///The name of the spell
+    let name: String
+    ///The level of the spell
+    let level: Int
+    ///The school of magic for the spell
+    let school: String
+    ///Indicates if the spell can be cast as ritual
+    let isRitual: Bool              = false
+    ///The time it takes to cast the spell in seconds
+    ///0 indicates the spell can be cast as a reaction
+    ///3 indicates the spell can be cast as a bonus action
+    let castTime: String
+    ///The maximum range of the spell in feet
+    ///-1 indicates the spell's range is infinite
+    ///0 indicates the spell can only be cast on self
+    let range: Int
+    ///Indicates if the spell requires the caster use concentration to maintain the spell for the duration
+    let requiresConcentration: Bool = false
+    ///The maxium duration the spell can last, in seconds
+    ///-1 indicates the spell lasts until it is dispelled
+    ///0 indicates that the spell is instantaneous and does not linger
+    let duration: String
+    ///The list of components that is required by the spell
+    ///V = Verbal, S = Somatic, M = Material
+    let components: String
+    ///The list of materials, if necessary
+    let materials: String?          = nil
+    ///The full description of the spell
+    let description: String
+    ///A description of the effect if the spell is cast using a slot above its level
+    let upcast: String?             = nil
+//    let target: Target
+//    enum Target {
+//        case area(Shape), creature(Int), caster
+//    }
+}
+
 
 struct Spell {
-	let level: Int
-	let name: String
-	let detail: String
-	let school: School
-	let maxRange: String
-	let castTime: (value: Int, metric: String)
-	let duration: (value: Int, metric: String)
-	let components: [Component]
-	let materials: String?
-	let spellSave: StatType?
-	let damage: Damage?
-	let shape: Shape?
-
-	var isCantrip: Bool {
-		return level == 0 ? true :  false }
-
-	static let maxLevel = 9
-
-	func damageForCantrip() -> Damage? {
-		guard let damage = damage else { print("no damage value"); return nil}
-		if level != 0 { print("Invalid Spell level: \(level) \(name)"); return damage}
-
-		let characterLevel 		= Character.current.level
-		var multiplier			= 1
-
-		if 		characterLevel >= 5  && characterLevel <= 10 	{ multiplier = 2 }
-		else if characterLevel >= 11 && characterLevel <= 16 	{ multiplier = 3 }
-		else if characterLevel >= 17 							{ multiplier = 4 }
-
-		return Damage(multiplier: multiplier, type: damage.type, value: damage.value)
-	}
-
 	enum School: String {
 		case abjuration, conjuration, divination, enchantment, illusion, evocation, necromancy, transmutation
 	}
 	enum Component: String {
 		case verbal, somatic, material
-	}
-}
-
-extension Spell {
-	struct Shape {
-		let size: Int
-		let style: Style
-
-		enum Style: String  {
-			case sphere, cylindar, cube, line, cone, point
-		}
-		static func fromString(_ string: String) -> Shape? {
-			let newString  = string.trimmingCharacters(in: .whitespacesAndNewlines)
-			//ideal format "00 shapeStyle"
-			guard newString.matches("^[0-9]+ .*") else { return nil }
-
-			guard
-				let sizeString		= newString.components(separatedBy: " ").first,
-				let shapeString		= newString.components(separatedBy: " ").last,
-				let size			= Int(sizeString),
-				let style			= Spell.Shape.Style(rawValue: shapeString)
-				else { print("Invalid spell shape format for string: \(string)"); return nil }
-
-			return Shape(size: size, style: style)
-		}
-	}
-}
-
-@objcMembers
-class SpellRecord: Object {
-	dynamic var id: String					= UUID().uuidString
-	dynamic var name: String				= "Spell Name"
-	dynamic var detail: String				= "Spell Description"
-	dynamic var higherLevelDetail: String?	= "Effect of spells cast at higher levels"
-	dynamic var availability: List<String>	= List<String>()
-	dynamic var level: Int					= 0
-	dynamic var school: String				= "SpellSchool"
-	dynamic var components: String			= "VSM"
-	dynamic var maxRange: String			= "000 feet"
-	dynamic var castTime: String			= "0 CastTime"
-	dynamic var duration: String			= "0 SpellDuration"
-	dynamic var concentration: Bool			= false  //change to requiresConcentration
-	dynamic var shape: String?				= "00 shapeStyle"
-	dynamic var materials: String?  		= "Materials"
-	dynamic var spellSave: String?			= "Stat Save"
-	dynamic var damage: String?				= "0d0 damageType"
-	dynamic var isRitual: Bool				= false
-
-	static func allRecords(in realm: Realm = RealmProvider.itemRecords.realm) -> Results<SpellRecord> {
-		return realm.objects(SpellRecord.self)//.sorted(byKeyPath: "name")
-	}
-
-	static func record(for name: String, in realm: Realm = RealmProvider.itemRecords.realm) -> SpellRecord? {
-		return allRecords().filter({ $0.name == name }).first
-	}
-
-	func spell() -> Spell {
-		return Spell(level: level, name: name, detail: detail, school: Spell.School(rawValue: school)!, maxRange: maxRange, castTime: tuple(forString: castTime), duration: tuple(forString: duration), components: components(from: components), materials: materials, spellSave: spellSave == nil ? nil : StatType(rawValue: spellSave!), damage: damage == nil ? nil : Damage.fromString(damage!), shape: shape == nil ? nil : Spell.Shape.fromString(shape!))
-	}
-	private func tuple(forString string: String) -> (value: Int, metric: String) {
-		let newString  = string.trimmingCharacters(in: .whitespacesAndNewlines)
-		guard
-			let valueString		= newString.components(separatedBy: " ").first,
-			let metric		 	= newString.components(separatedBy: " ").last,
-			let value			= Int(valueString)
-			else { print("Invalid format in \(name.capitalized) for string: \(string)"); return (value: 0, metric: string) }
-
-		return (value:  value, metric: metric)
-	}
-	private func components(from string: String) -> [Spell.Component] {
-		var result = [Spell.Component]()
-		let modifiedString = string.lowercased()
-		if modifiedString == "" || !modifiedString.matches("^(v|s|m)+") { return result }
-		if modifiedString.contains("v") { result.append(.verbal)   	}
-		if modifiedString.contains("m") { result.append(.material) 	}
-		if modifiedString.contains("s") { result.append(.somatic)	}
-
-		return result
 	}
 }
