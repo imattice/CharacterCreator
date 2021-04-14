d6352a8ddf7a3accc849d471c9285504d5b362c0
Ike Mattice <ikemattice@icloud.com>
2021-04-13 19:31:16 -0700

    Update file structure to keep UIKit and SwiftUI code separate; Remove support for iPhone; Update build settings to support iPadOS 14.4; Add @main SourceApp struct for SwiftUI

---
+++ CharacterCreator/SwiftUI/App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceApp.swift
+//  Shared
+//
+//  Created by Ike Mattice on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct SourceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CompendiumView()
+        }
+    }
+}
---
+++ CharacterCreator/SwiftUI/CompendiumView.swift
@@ -0,0 +1,21 @@
+//
+//  CompendiumView.swift
+//  CharacterCreator
+//
+//  Created by Ike Mattice on 4/13/21.
+//  Copyright © 2021 Ike Mattice. All rights reserved.
+//
+
+import SwiftUI
+
+struct CompendiumView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CompendiumView_Previews: PreviewProvider {
+    static var previews: some View {
+        CompendiumView()
+    }
+}
