db3317e3228063601b14d1a6f0e47f1d14a54eb4
Ike Mattice <ikemattice@icloud.com>
2021-07-01 08:38:28 -0700

    Feat: Load Record data on app initialization

--- CharacterCreator/SwiftUI/App.swift
+++ CharacterCreator/SwiftUI/App.swift
@@ -12,6 +12,10 @@ struct SourceApp: App {
     @Environment(\.scenePhase) var scenePhase
     let dataStack = CoreDataStack.shared
     
+    init() {
+        loadDataFromJSON()
+    }
+    
     var body: some Scene {
         WindowGroup {
             CompendiumView()
@@ -19,6 +23,35 @@ struct SourceApp: App {
         }
         .onChange(of: scenePhase) { _ in
             dataStack.save()
+        }
+    }
+    
+    func loadDataFromJSON() {
+        do {
+            try RaceRecord.save()
+            try SubraceRecord.save()
+            try ClassRecord.save()
+            try SubclassRecord.save()
+            try BackgroundRecord.save()
+            try FeatRecord.save()
+            try LanguageRecord.save()
+            try ItemRecord.save()
+            try WeaponRecord.save()
+            try ArmorRecord.save()
+            try ShieldRecord.save()
+            try PackRecord.save()
+            try ToolRecord.save()
+            try SpellRecord.save()
+            try MagicItemRecord.save()
+            try RuleRecord.save()
+            try ConditionRecord.save()
+            try DiseaseRecord.save()
+            try PoisonRecord.save()
+            try TrapRecord.save()
+            try CreatureRecord.save()
+        }
+        catch {
+            print(error)
         }
     }
 }
